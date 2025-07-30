c882533842b404e7e69223ebe5bf61fd0ced1a38
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-07-30 12:51:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <h2>Button Counter</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <p>Button clicked {count} times</p>
     </div>
   )
 }
